52966a534bdf0d7fe75a6b7acea6ab02ae079c60
vietson6103@gmail.com <Hn0912388240>
2018-08-10 09:31:08 +0700

    add method listCustomers

--- src/main/java/controller/CustomerServlet.java
+++ src/main/java/controller/CustomerServlet.java
@@ -1,13 +1,16 @@
 package controller;
 
+import model.Customer;
 import service.CustomerService;
 import service.CustomerServiceImpl;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "CustomerServlet")
 public class CustomerServlet extends HttpServlet {
@@ -46,7 +49,22 @@ public class CustomerServlet extends HttpServlet {
             case "view":
                 break;
             default:
+                listCustomers(request, response);
                 break;
+        }
+    }
+
+    private void listCustomers(HttpServletRequest request, HttpServletResponse response) {
+        List<Customer> customers = this.customerService.findAll();
+        request.setAttribute("customers", customers);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/list.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
